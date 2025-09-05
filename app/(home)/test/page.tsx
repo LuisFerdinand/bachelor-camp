edfd1b7423cdc16d776517d24efb73be59297636
VincentKon <vincent373kon@gmail.com>
2025-09-05 17:45:18 +0700

    add dynamic const

--- app/(home)/test/page.tsx
+++ app/(home)/test/page.tsx
@@ -1,7 +1,12 @@
+export const dynamic = "force-dynamic";
 import React, { Suspense } from "react";
 import { HydrateClient, trpc } from "@/trpc/server";
 import { ErrorBoundary } from "react-error-boundary";
 import { PageClient } from "./client";
+
+// app/test/page.tsx
+// or
+export const revalidate = 0;
 
 const Test = () => {
   return (
