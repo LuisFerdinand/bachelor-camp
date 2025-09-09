7f11adab58448a1ea3fc53bfc143e76f23b17d4a
LuisFerdinand <ferdinandluis88@gmail.com>
2025-09-09 22:30:09 +0700

    add suspense in booking

--- app/(home)/booking/page.tsx
+++ app/(home)/booking/page.tsx
@@ -1,5 +1,11 @@
 "use client";
-
+import { Suspense } from 'react';
 import BookingPage from "@/modules/home/ui/pages/booking";
 
-export default BookingPage;
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading booking information...</div>}>
+      <BookingPage />
+    </Suspense>
+  );
+}
