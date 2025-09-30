5367f8b72f94fa73eaa333eb237a671a764f0a25
luisferdinandwork <luis.ferdinand@panatrade.id>
2025-09-30 19:00:52 +0700

    wrap with suspense

--- app/(home)/program-booking/page.tsx
+++ app/(home)/program-booking/page.tsx
@@ -1,5 +1,10 @@
 import ProgramBooking from "@/modules/home/ui/pages/programBooking";
+import { Suspense } from "react";
 
 export default function ProgramBookingPage() {
-  return <ProgramBooking />;
+  return (
+    <Suspense fallback={<div>Loading booking information...</div>}>
+      <ProgramBooking />
+    </Suspense>
+  );
 }
