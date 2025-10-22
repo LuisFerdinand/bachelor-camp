841859116f3054ecad0044c6241c325ccfbc67f9
luisferdinandwork <luis.ferdinand@panatrade.id>
2025-10-22 15:31:20 +0700

    try remove suppressHydrationWarning

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className} suppressHydrationWarning>
+        <body className={inter.className}>
           <TRPCProvider>
             <Toaster position="bottom-right"></Toaster>
             {children}
